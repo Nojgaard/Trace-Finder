f886079dcb36c31d1aeded8c2f8593d262ce34f8
Nikolai <nn@appworkers.dk>
2015-11-25 15:33:37 +0100

    Made the base graph and parser logic.

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include "graph/base.hpp"
+#include "graph/io.hpp"
+#include <exception>
+
+using namespace std;
+using namespace graph;
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "Generate help message.")
+		("input,i", po::value<string>()->required(),
+		 	"Path of the input file.")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc,argv,desc),vm);
+
+	if (vm.count("help")) {
+		cout << desc << endl;
+		return 0;
+	}
+	string file_path = vm["input"].as<string>();
+	graph_t g;
+	try {
+		io::file::read(file_path, g);
+	} catch (exception &e) {
+		cout << e.what() << endl;
+		return 0;
+	}
+
+	return 0;
+}
